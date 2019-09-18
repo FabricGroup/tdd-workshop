a5c9853f9d4288f8d1bdaf4b53cd9a963343dd40
piggy <mohnish.chowdhury@gmail.com>
2019-09-18 20:53:34 +1000

    our first test

---
+++ HelloWorldTests/GreeterTests.cs
@@ -0,0 +1,23 @@
+﻿using HelloWorld;
+using NUnit.Framework;
+
+namespace HelloWorldTests
+{
+    // command line run: dotnet.exe test .\HelloWorldTests\HelloWorldTests.csproj
+    [TestFixture]
+    public class GreeterTests
+    {
+        [Test]
+        public void ShouldGreetWithThePersonsNameIfTheNameIsProvided()
+        {
+            //Setup
+            var greeter = new Greeter(); 
+
+            //Act
+            var greeting = greeter.Greet("John"); 
+            
+            //Assert
+            Assert.That(greeting, Is.EqualTo("Hello John!")); 
+        }
+    }
+}
