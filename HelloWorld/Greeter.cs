85e908900d9793e41b88735b3a15741e662f0c42
piggy <mohnish.chowdhury@gmail.com>
2019-09-30 17:04:47 +1000

    adding and when giving multiple peoples names

--- HelloWorld/Greeter.cs
+++ HelloWorld/Greeter.cs
@@ -1,11 +1,19 @@
-﻿namespace HelloWorld
+﻿using System.Linq;
+
+namespace HelloWorld
 {
     public class Greeter
     {
         public string Greet(params string[] names)
         {
-            var peopleToGreet = names.Length == 0 ? "World" : string.Join(", ", names);
+            var peopleToGreet = names.Length == 0 ? "World" : PeopleToGreet(names);
             return $"Hello {peopleToGreet}!";
+        }
+
+        private static string PeopleToGreet(string[] names)
+        {
+            if (names.Length == 1) return names[0];
+            return $"{string.Join(", ", names.Take(names.Length - 1))}, and {names.Last()}";
         }
     }
 }
